26e44c4c7b23ffd50825e1882135fbb65f6b2048
João Pedro Rocha <jpcrocha8@gmail.com>
2024-04-17 08:38:11 -0300

    feat: implements users controllers

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -6,80 +6,112 @@ import UsersRepository from "../models/users/UsersRepository.js";
 const usersRepository = new UsersRepository();
 
 export const getUsers = async (req, res) => {
-  const users = await usersRepository.getUsers();
+  try {
+    const users = await usersRepository.getUsers();
 
-  if (!users) {
-    return res.status(404).send({ message: "Não há usuários cadastrados" });
+    if (!users) {
+      return res.status(404).send({ message: "Não há usuários cadastrados" });
+    }
+    return res.status(200).send({ totalUsers: users.length, users });
+  } catch (error) {
+    return res.status(500).send({
+      message:
+        "Erro ao buscar os dados dos usuarios:", error: error.message
+    })
   }
-  return res.status(200).send({ totalUsers: users.length, users });
 };
 
 export const getUserById = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const user = await usersRepository.getUserById(id);
+    const user = await usersRepository.getUserById(id);
 
-  if (!user) {
-    return res.status(404).send({ message: "Usuário não encontrado" });
+    if (!user) {
+      return res.status(404).send({ message: "Usuário não encontrado" });
+    }
+
+    return res.status(200).send({ message: "Usuário encontrado", user });
+  } catch (error) {
+    return res.status(500)
+      .send({ message: "Erro ao buscar o usuario por ID:", error: error.message });
   }
-
-  return res.status(200).send({ message: "Usuário encontrado", user });
 };
 
 export const createUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  const userAlreadyExists = await usersRepository.getUserByEmail(email);
+    const userAlreadyExists = await usersRepository.getUserByEmail(email);
 
-  if (userAlreadyExists) {
-    return res.status(409).send({ message: "Usuário já cadastrado" });
+    if (userAlreadyExists) {
+      return res.status(409).send({ message: "Usuário já cadastrado" });
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    const user = new User(name, email, passwordHash);
+
+    await usersRepository.createUser(user);
+
+    return res.status(201).send({ message: "Usuário criado com sucesso", user });
+  } catch (error) {
+    return res.status(500)
+      .send({
+        message:
+          "Erro no cadastro de um novo usuário: ", error: error.message
+      });
   }
-
-  const passwordHash = await hash(password, 8);
-
-  const user = new User(name, email, passwordHash);
-
-  await usersRepository.createUser(user);
-
-  return res.status(201).send({ message: "Usuário criado com sucesso", user });
 };
 
 export const updateUser = async (req, res) => {
-  const { id } = req.params;
-  const { name, email, password } = req.body;
+  try {
+    const { id } = req.params;
+    const { name, email, password } = req.body;
 
-  const userById = await usersRepository.getUserById(id);
-  const userByEmail = await usersRepository.getUserByEmail(email);
+    const userById = await usersRepository.getUserById(id);
+    const userByEmail = await usersRepository.getUserByEmail(email);
 
-  if (!userById) {
-    return res.status(404).send({ message: "Usuário não encontrado" });
+    if (!userById) {
+      return res.status(404).send({ message: "Usuário não encontrado" });
+    }
+
+    if (userByEmail && userByEmail.id !== id) {
+      return res.status(409).send({ message: "Email já cadastrado" });
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    const user = await usersRepository.updateUser(id, name, email, passwordHash);
+
+    return res
+      .status(200)
+      .send({ message: "Usuário atualizado com sucesso", user });
+  } catch (error) {
+    return res.status(500)
+      .send({
+        message:
+          "Erro na atualização do usuário:", error: error.message
+      })
   }
-
-  if (userByEmail && userByEmail.id !== id) {
-    return res.status(409).send({ message: "Email já cadastrado" });
-  }
-
-  const passwordHash = await hash(password, 8);
-
-  const user = await usersRepository.updateUser(id, name, email, passwordHash);
-
-  return res
-    .status(200)
-    .send({ message: "Usuário atualizado com sucesso", user });
 };
 
 export const deleteUser = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const user = await usersRepository.getUserById(id);
+    const user = await usersRepository.getUserById(id);
 
-  if (!user) {
-    return res.status(404).send({ message: "Usuário não encontrado" });
+    if (!user) {
+      return res.status(404).send({ message: "Usuário não encontrado" });
+    }
+
+    await usersRepository.deleteUser(id);
+
+    return res
+      .status(200)
+      .send({ message: "Usuário deletado com sucesso", user });
+  } catch (error) {
+    res.status(500).send({ message: "Erro ao deletar o usuário", error: error.message });
   }
-
-  await usersRepository.deleteUser(id);
-
-  return res
-    .status(200)
-    .send({ message: "Usuário deletado com sucesso", user });
 };
